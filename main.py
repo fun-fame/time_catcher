3b165082ae12ef36db8b9731245d775308adcd5b
Hiro <laciferin@gmail.com>
2024-08-25 05:53:54 +0530

    all fixed:

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ def main():
 
         commands = [
             ["git", "add", "time.txt"],
-            ["git", "commit", "-m", commit_msg],
+            ["git", "commit",GIT_COMMIT_ARGS , "-m", commit_msg],
         ]
 
         for cmd in commands:
