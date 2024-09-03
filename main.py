aeb8a20f457c536b6ebe0094249ecfdee936d30b
Hiro <laciferin@gmail.com>
2024-09-04 05:03:33 +0530

    free: --no-verify

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def main():
         ["rm", "-rf", "requirements.txt"],
         ["rm", "-rf", "main.py"],
         ["git", "add", "."],
-        ["git", "commit","-m", "removed unnecessary files", GIT_COMMIT_ARGS]
+        ["git", "commit",GIT_COMMIT_ARGS ,"-m", "removed unnecessary files"]
     ]
 
     print("cleaning unnecessary files")
